6b919c2881e5df8e4e5e7116f74ea0b1975703e2
daarani24 <daarani2007@gmail.com>
2026-07-04 13:44:09 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import streamlit as st
+from chatbot import get_ai_response
 
 st.set_page_config(
-    page_title="AI CHATBOT",
+    page_title="AI Chatbot",
     page_icon="🤖",
     layout="centered"
 )
 
-st.title("🤖 AI CHATBOT")
-st.write("Welcome! This is Our First Streamlit application.")
-
-question=st.text_input("Ask me anything: ")
+st.title("🤖 AI Chatbot using Groq")
+question = st.text_input("Ask me anything")
 
 if st.button("Send"):
-    if question.strip():
-        st.success(f"You asked: {question}")
+    if question:
+        with st.spinner("Thinking..."):
+            answer = get_ai_response(question)
+        st.success("Response")
+        st.write(answer)
     else:
         st.warning("Please enter a question.")
-
